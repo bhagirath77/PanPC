41aa1df25f1756c983b6ec6675e048775c0e17b1
Bhagirath Sarvaiya <sarvaiya.1@iitj.ac.in>
2022-10-07 05:22:16 +0530

    Ignore

---
+++ 2.py
@@ -0,0 +1,26 @@
+def getMinMax(arr):
+	
+	n = len(arr)
+	if(n % 2 == 0):
+		mx = max(arr[0], arr[1])
+		mn = min(arr[0], arr[1])
+		i = 2
+	else:
+		mx = mn = arr[0]
+		i = 1
+	while(i < n - 1):
+		if arr[i] < arr[i + 1]:
+			mx = max(mx, arr[i + 1])
+			mn = min(mn, arr[i])
+		else:
+			mx = max(mx, arr[i])
+			mn = min(mn, arr[i + 1])
+		i += 2
+	
+	return (mx, mn)
+	
+if __name__ =='__main__':
+	
+	arr = [1000, 11, 445, 1, 330, 3000]
+	mx, mn = getMinMax(arr)
+	
